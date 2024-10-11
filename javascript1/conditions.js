169eea3b014dd311ed651f4df6600119928822a6
Espace NUMERIQUE 12 <espace12@numerique.lan>
2024-10-11 11:30:52 +0300

    creation of conditions.js

---
+++ javascript1/conditions.js
@@ -0,0 +1,34 @@
+// Les conditions
+
+/* Si A existe, alors B s'affiche*/
+
+// "If" (condition)
+
+
+
+let note = 5 ;
+
+if (note = 5);
+{ 
+    alert ( "insuffisant")
+};
+
+if(note = 10) ;
+{
+    alert ("passable");
+};
+
+if (note = 12);
+{
+    alert ("assez bien")
+};
+
+if (note = 16);
+{
+    alert ("bien")
+};
+
+if (note = 18);
+{
+    alert ("très bien")
+};
